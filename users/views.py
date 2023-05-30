3e2ba92f761c7368e79cfdf154a02c704de5b073
Mombinya <bigshemo@gmail.com>
2023-05-30 12:28:25 +0300

    Mfua nguo

---
+++ users/views.py
@@ -0,0 +1,45 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+from .form import RegisterUserForm
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Account_created. Please login')
+            return redirect('login')
+        
+        else:
+            messages.warning(request, "Sorry. Something went wrong")
+    else:
+        form = RegisterUserForm()
+        context = {'form':form}
+        return render(request, 'users/register.html', context)
+    
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect('home')
+        
+        else:
+            messages.warning(request, 'Sorry, something went wrong')
+            return redirect(login)
+        
+    else:
+        return render(request, 'users/login.html')
+    
+def logout_user(request):
+    logout(request)
+    return redirect
+def landing(request):
+    return render(request, 'users/landing.html')
