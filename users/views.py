1859ac8bb241688cb2824fa0caf3de51e0855054
Mombinya <bigshemo@gmail.com>
2023-06-12 13:07:19 +0300

    vua nifue

--- users/views.py
+++ users/views.py
@@ -6,19 +6,30 @@ from users.form import RegisterUserForm
 
 
 def register_user(request):
+    print("Inside register_user view")  # Debug statement
+
     if request.method == 'POST':
+        print("POST request received")  # Debug statement
+
         form = RegisterUserForm(request.POST)
         if form.is_valid():
+            print("Form is valid")  # Debug statement
+
             form.save()
             messages.info(request, 'Account created. Please login')
             return redirect('login')
         else:
+            print("Form is invalid")  # Debug statement
+
             messages.warning(request, "Sorry. Something went wrong")
     else:
+        print("GET request received")  # Debug statement
+
         form = RegisterUserForm()
 
-        context = {'form':form}
-        return render(request, 'users/register.html', context)
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
+
     
 def login_user(request):
     if request.method == 'POST':
