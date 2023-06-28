df6d2cf5ae54a6b9731c3aa99bd2ced061a4459d
Mombinya <bigshemo@gmail.com>
2023-06-28 07:56:26 +0300

    another wash

--- Laundry/urls.py
+++ Laundry/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('add-service/', views.add_service, name='add-service'),
     path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
     path('about/', views.about, name='about'), 
-    path('add-services-to-cart/', views.add_services_to_cart, name='add-services-to-cart')
+    path('add-services-to-cart/', views.add_services_to_cart, name='add-services-to-cart'),
+    path('delete-service-from-cart/<int:pk>/', views.delete_service_from_cart, name='delete-service-from-cart')
 ]
--- Laundry/views.py
+++ Laundry/views.py
@@ -14,25 +14,22 @@ def landing(request):
     return render(request, 'laundry/landing.html', context)
 
 def about(request):
-    context={}
+    context = {}
     return render(request, 'laundry/about.html', context)
-
-from django.contrib import messages
-
 
 def add_service(request):
     if request.method == 'POST':
         form = AddServiceForm(request.POST)
-
         if form.is_valid():
-            var = form.save(commit=False)
-            var.user = request.user
-            var.save()
+            service = form.save(commit=False)
+            service.user = request.user
+            service.save()
             messages.success(request, 'New service added successfully!')
             return redirect('add-service')
         else:
             messages.warning(request, 'Sorry, something went wrong.')
             return redirect('add-service')
+
     else:
         form = AddServiceForm()
         context = {'form': form}
@@ -41,13 +38,12 @@ def add_service(request):
 
 def add_to_cart(request):
     if request.method == 'POST':
-
         form = AddToCartForm(request.POST)
         if form.is_valid():
-            var = form.save(commit=False)
-            var.user = request.user
-            var.save()
-            request.session['cart_id'] = var.id
+            cart_item = form.save(commit=False)
+            cart_item.user = request.user
+            cart_item.save()
+            request.session['cart_id'] = cart_item.id
             return redirect('add-services-to-cart')
         else:
             messages.warning(request, 'Sorry, Something went wrong!')
@@ -62,28 +58,42 @@ def add_services_to_cart(request):
     if request.method == 'POST':
         form = AddServiceToCartForm(request.POST)
         if form.is_valid():
-            var = form.save(commit=False)
-            var.user = request.user
+            service_cart_item = form.save(commit=False)
+            service_cart_item.user = request.user
             cart = Cart.objects.get(id=request.session['cart_id'])
-            var.cart = cart
-            var.save()
+            service_cart_item.cart = cart
+            service_cart_item.save()
             get_obj = ServiceCart.objects.filter(cart=cart)
             count = 0
             for obj in get_obj:
                 calc = obj.service.price * cart.clothes_amount
-                count = count+calc
+                count += calc
             cart.total_amount = count
             cart.save()
             messages.info(request, 'A service has been added to your cart')
             return redirect('add-services-to-cart')
         else:
             print(form.errors)
-            messages.warning(request, 'something went wrong')
+            messages.warning(request, 'Something went wrong')
             return redirect('add-services-to-cart')
         
     else:
-        form=AddServiceToCartForm()
+        form = AddServiceToCartForm()
         cart = Cart.objects.get(id=request.session['cart_id'])
         get_obj = ServiceCart.objects.filter(cart=cart)
-        context = {'form':form, 'get_obj':get_obj}
+        context = {'form': form, 'get_obj': get_obj}
         return render(request, 'laundry/add_service_to_cart.html', context) 
+    
+def delete_service_from_cart(request, pk):
+    service_cart_item = ServiceCart.objects.get(pk=pk)
+    service_cart_item.delete()
+
+    cart = Cart.objects.get(id=request.session['cart_id'])
+    get_obj = ServiceCart.objects.filter(cart=cart)
+    count = 0
+    for obj in get_obj:
+        calc = obj.service.price * cart.clothes_amount
+        count += calc
+    cart.total_amount = count
+    cart.save()
+    return redirect('add-services-to-cart')
